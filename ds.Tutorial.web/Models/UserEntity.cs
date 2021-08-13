b5f0dc9d91e292af658a7760c82ec9f288f4a84a
kenshu <kensyu124logic@yahoo.co.jp>
2021-08-13 17:15:08 +0900

    fix

---
+++ ds.Tutorial.web/Models/UserEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ds.Tutorial.web.Models
+{
+    [Table("user")]
+    [Serializable]
+    public class UserEntity
+    {
+        // 主キー
+        [Key]
+        // インクリメント
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        // カラム名
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("age")]
+        public int Age { get; set; }
+
+        [Column("hobby")]
+        public string Hobby { get; set; }
+    }
+}
