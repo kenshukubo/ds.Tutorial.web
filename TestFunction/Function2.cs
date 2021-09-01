3219621c4dc0a5a61456d56f4e748ce8b0b85a28
kenshu <kensyu124logic@yahoo.co.jp>
2021-09-01 10:32:29 +0900

    DBからUserテーブル取得する

---
+++ TestFunction/Function2.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestFunction
+{
+    public class Function2
+    {
+        public Function2(TutorialDbContext tutorialDbContext)
+        {
+            _tutorialDbContext = tutorialDbContext;
+        }
+
+        private readonly TutorialDbContext _tutorialDbContext;
+
+        [FunctionName("Function2")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation($".NET Core Version: {Environment.Version}");
+
+            var list = await _tutorialDbContext.Users.ToListAsync();
+
+            return new OkObjectResult(list);
+        }
+    }
+}
