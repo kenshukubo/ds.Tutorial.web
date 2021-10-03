e3361243e7c6c8063c97449d6062083f26f6fae9
kenshu <kensyu124logic@yahoo.co.jp>
2021-10-03 17:12:11 +0900

    動作OK ローカルへアップロード

--- ds.Tutorial.web/Controllers/WorkController.cs
+++ ds.Tutorial.web/Controllers/WorkController.cs
@@ -12,42 +12,77 @@ namespace ds.Tutorial.web.Controllers
 {
     public class WorkController : Controller
     {
+        //こっちだと動作しない
+        //public IActionResult Index()
+        //{
+        //    Console.WriteLine("画面表示");
+        //    return View(new CreatePost());
+        //}
+
+        //private readonly IHostingEnvironment hostingEnvironment;
+        //public WorkController(IHostingEnvironment environment)
+        //{
+        //    hostingEnvironment = environment;
+        //}
+
+        //[HttpPost]
+        //public IActionResult FileUpload(CreatePost model)
+        //{
+        //    Console.WriteLine("あっぷろーど");
+
+        //    // do other validations on your model as needed
+        //    if (model.MyImage != null)
+        //    {
+        //        var uniqueFileName = GetUniqueFileName(model.MyImage.FileName);
+        //        var uploads = Path.Combine(hostingEnvironment.WebRootPath, "uploads");
+        //        var filePath = Path.Combine(uploads, uniqueFileName);
+        //        model.MyImage.CopyTo(new FileStream(filePath, FileMode.Create));
+        //        Console.WriteLine("あっぷろーど完了");
+        //    }
+
+        //    return RedirectToAction("Index", "Work");
+        //}
+
+        //private string GetUniqueFileName(string fileName)
+        //{
+        //    fileName = Path.GetFileName(fileName);
+        //    return Path.GetFileNameWithoutExtension(fileName)
+        //              + "_"
+        //              + Guid.NewGuid().ToString().Substring(0, 4)
+        //              + Path.GetExtension(fileName);
+        //}
+
         public IActionResult Index()
         {
-            Console.WriteLine("画面表示");
-            return View(new CreatePost());
-        }
-
-        private readonly IHostingEnvironment hostingEnvironment;
-        public WorkController(IHostingEnvironment environment)
-        {
-            hostingEnvironment = environment;
+            return View();
         }
 
         [HttpPost]
-        public IActionResult FileUpload(CreatePost model)
+        public async Task<IActionResult> FileUpload(List<IFormFile> files)
         {
+            // 保存先を取得
+            string filePath = @"C:\work\";
+
             Console.WriteLine("あっぷろーど");
 
-            // do other validations on your model as needed
-            if (model.MyImage != null)
+            // ファイル保存
+            long size = files.Sum(f => f.Length);
+            foreach (var formFile in files)
             {
-                var uniqueFileName = GetUniqueFileName(model.MyImage.FileName);
-                var uploads = Path.Combine(hostingEnvironment.WebRootPath, "uploads");
-                var filePath = Path.Combine(uploads, uniqueFileName);
-                model.MyImage.CopyTo(new FileStream(filePath, FileMode.Create)); 
+                Console.WriteLine("あっぷろーど開始");
+                if (formFile.Length <= 0) continue;
+                // ローカルに保存
+                using (var stream = new FileStream(filePath + formFile.FileName, FileMode.Create))
+                {
+                    await formFile.CopyToAsync(stream);
+                }
             }
 
-            return RedirectToAction("Index", "Work");
-        }
+            // 画面に返す返却値
+            ViewData["uploadResult"] = Ok(new { count = files.Count, size, filePath }).Value.ToString();
 
-        private string GetUniqueFileName(string fileName)
-        {
-            fileName = Path.GetFileName(fileName);
-            return Path.GetFileNameWithoutExtension(fileName)
-                      + "_"
-                      + Guid.NewGuid().ToString().Substring(0, 4)
-                      + Path.GetExtension(fileName);
+            // ViewをIndex画面で返却
+            return View("Index");
         }
     }
 }
